6a6aea2b320d45f609e575428ed5945b252e9456
Poorvi705 <gpoorvi288@gmail.com>
2026-01-15 22:33:21 +0530

    Assignment 2 Problem 19

---
+++ Basic Programs/SquareSideFromPerimeter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SquareSideFromPerimeter {
+    public static void main(String[] args) {
+
+        double perimeter;
+        double side;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the perimeter of the square: ");
+        perimeter = input.nextDouble();
+        side = perimeter / 4;
+
+        System.out.println(
+            "The length of the side is " + side +
+            " whose perimeter is " + perimeter );
+
+        input.close();
+    }
+}
