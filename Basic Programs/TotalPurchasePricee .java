634d34a8244a899a3849389b4677cda9a71b6dd2
Poorvi705 <gpoorvi288@gmail.com>
2026-01-15 22:45:22 +0530

    Assignment 2 Problem 21

---
+++ Basic Programs/TotalPurchasePricee .java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TotalPurchasePricee {
+    public static void main(String[] args) {
+        double unitPrice;
+        int quantity;
+        double totalPrice;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter unit price (INR): ");
+        unitPrice = input.nextDouble();
+
+        System.out.print("Enter quantity: ");
+        quantity = input.nextInt();
+        totalPrice = unitPrice * quantity;
+        System.out.println(
+            "The total purchase price is INR " + totalPrice +
+            " if the quantity " + quantity +
+            " and unit price is INR " + unitPrice);
+        input.close();
+    }
+}
