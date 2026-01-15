480160c7b97f447a53ed40feb9499aa65cb723bf
Poorvi705 <gpoorvi288@gmail.com>
2026-01-15 22:40:40 +0530

    Assignment 2 Problem 20

---
+++ Basic Programs/FeetToYardsMiless.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FeetToYardsMiless {
+    public static void main(String[] args) {
+
+        double distanceInFeet;
+        double distanceInYards;
+        double distanceInMiles;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the distance in feet: ");
+        distanceInFeet = input.nextDouble();
+        distanceInYards = distanceInFeet / 3;        
+        distanceInMiles = distanceInYards / 1760;    
+
+        System.out.println(
+            "The distance in yards is " + distanceInYards +
+            " while the distance in miles is " + distanceInMiles  );
+
+        input.close();
+    }
+}
