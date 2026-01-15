d590a30c3dd1d816c4e17b393eb4ef43ed1e4bf6
Poorvi705 <gpoorvi288@gmail.com>
2026-01-15 22:00:24 +0530

    Assignment 2 Problem 11

---
+++ Basic Programs/BasicCalculator .java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+
+        double number1;
+        double number2;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        number1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        number2 = input.nextDouble();
+        double sum = number1 + number2;
+        double difference = number1 - number2;
+        double product = number1 * number2;
+        double quotient = number1 / number2;
+
+       
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+
+        input.close();
+    }
+}
