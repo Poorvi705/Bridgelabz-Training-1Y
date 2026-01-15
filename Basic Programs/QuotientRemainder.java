b3e01e775bf785f312f06cbd4b0512ff3d1028b9
Poorvi705 <gpoorvi288@gmail.com>
2026-01-15 22:48:34 +0530

    Assignment 2 Problem 22

---
+++ Basic Programs/QuotientRemainder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+
+        int number1, number2;
+        int quotient, remainder;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        number2 = input.nextInt();
+        quotient = number1 / number2;
+        remainder = number1 % number2;
+        System.out.println(
+            "The Quotient is " + quotient +
+            " and Reminder is " + remainder +
+            " of two numbers " + number1 + " and " + number2 );
+
+        input.close();
+    }
+}
