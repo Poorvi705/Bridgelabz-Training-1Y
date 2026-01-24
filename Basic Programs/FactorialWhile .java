c6f0d44e426724a5340c70d6ae00cf62fa904919
Poorvi705 <gpoorvi288@gmail.com>
2026-01-24 16:53:13 +0530

    Assignment 3 Problem 14

---
+++ Basic Programs/FactorialWhile .java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FactorialWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        
+        if (num >= 0) {
+            long factorial = 1;
+            int i = 1;
+
+            
+            while (i <= num) {
+                factorial *= i;
+                i++;
+            }
+
+            System.out.println("The factorial of " + num + " is " + factorial);
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+
+        sc.close();
+    }
+}
