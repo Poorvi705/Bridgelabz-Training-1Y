4afe383b18c830abc460f42b3c6bb9cc70c53e4b
Poorvi705 <gpoorvi288@gmail.com>
2026-01-24 16:45:11 +0530

    Assignment 3 Problem 13

---
+++ Basic Programs/SumOfNaturalNumbersForLoop.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbersForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        
+        if (n > 0) {
+
+           
+            int sumFor = 0;
+            for (int i = 1; i <= n; i++) {
+                sumFor += i;
+            }
+
+         
+            int sumFormula = n * (n + 1) / 2;
+
+            
+            System.out.println("Sum using for loop: " + sumFor);
+            System.out.println("Sum using formula: " + sumFormula);
+
+            
+            if (sumFor == sumFormula) {
+                System.out.println("Both computations are correct.");
+            } else {
+                System.out.println("Both computations are not correct.");
+            }
+
+        } else {
+            System.out.println("The number " + n + " is not a natural number.");
+        }
+
+        sc.close();
+    }
+}
