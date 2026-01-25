54a0fd44ce9e69ff5870ebddf5c9497f61363817
Poorvi705 <gpoorvi288@gmail.com>
2026-01-25 23:06:37 +0530

    Assignment 3.1 Problem 5

--- Basic Programs/FizzBuzzWhile.java
+++ Basic Programs/FizzBuzzWhile.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
-public class FizzBuzz {
+public class FizzBuzzWhile {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
 
+        
         if (number <= 0) {
             System.out.println("Please enter a positive integer.");
         } else {
+            int i = 1;
+
             
-            for (int i = 1; i <= number; i++) {
+            while (i <= number) {
                 if (i % 3 == 0 && i % 5 == 0) {
                     System.out.println("FizzBuzz");
                 } else if (i % 3 == 0) {
@@ -22,6 +26,7 @@ public class FizzBuzz {
                 } else {
                     System.out.println(i);
                 }
+                i++;
             }
         }
 
