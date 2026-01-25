8b9432c40942db64483489578f17a1b43285b88a
Poorvi705 <gpoorvi288@gmail.com>
2026-01-26 00:03:33 +0530

    Assignment 3.1 Problem 14

---
+++ Basic Programs/PowerUsingWhile.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PowerUsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Enter the power (positive integer): ");
+        int power = sc.nextInt();
+
+        if (power < 0) {
+            System.out.println("Power should be a positive integer.");
+        } else {
+            int result = 1;    
+            int counter = 0;     
+
+            while (counter < power) {
+                result = result * number;
+                counter++;       
+            }
+            System.out.println(number + " raised to the power " + power + " is " + result);
+        }
+
+        sc.close();
+    }
+}
