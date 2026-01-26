93490db30512aba1b7eb8e3dfd4c55f0e9fba6ce
Poorvi705 <gpoorvi288@gmail.com>
2026-01-26 20:55:30 +0530

    Assignment 3.2 Problem 7

---
+++ Basic Programs/BMICalculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    
+        System.out.print("Enter weight in kg: ");
+        double weight = sc.nextDouble();
+
+        System.out.print("Enter height in cm: ");
+        double heightCm = sc.nextDouble();
+
+ 
+        double heightMeter = heightCm / 100;
+
+        double bmi = weight / (heightMeter * heightMeter);
+
+        String status;
+
+        
+        if (bmi <= 18.4) {
+            status = "Underweight";
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            status = "Normal";
+        } else if (bmi >= 25.0 && bmi <= 39.9) {
+            status = "Overweight";
+        } else {
+            status = "Obese";
+        }
+
+      
+        System.out.println("\nBMI = " + bmi);
+        System.out.println("Weight Status: " + status);
+    }
+}
